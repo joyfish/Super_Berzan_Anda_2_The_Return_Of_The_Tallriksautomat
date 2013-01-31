0ab4cfa2278375ecc86d68d8019e25064593af9c
Rydfalk <v.rydfalk@gmail.com>
2013-01-31 14:02:16 +0100

    Added a new class and package, 'handlers' and ProjectileCalc.
    The 'missile' should follow an arc instead of a line. This class will
    calculate the trajectory of the missile.

--- PA_2/src/entities/Missile.java
+++ PA_2/src/entities/Missile.java
@@ -10,47 +10,70 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Missile {
 	private Texture texture;
-	private Vector2 position;	
-	private Vector2 speed;	
+	private Vector2 position;
+	private Vector2 speed;
 	private Sprite sprite;
 	private Vector2 offset;
-	
-	public Missile(Vector2 startPosition, Vector2 playerPos, GameScreen master){
+
+	public Missile(Vector2 startPosition, Vector2 playerPos, GameScreen master) {
 		position = startPosition;
-		if(playerPos.x  > startPosition.x + master.getBackground().getOffset().x ){
-			speed = new Vector2(2,0);
-			position.add(32,0);
+		if (playerPos.x > startPosition.x
+				+ master.getBackground().getOffset().x) {
+			speed = new Vector2(2, 0);
+			position.add(32, 0);
 		} else {
-			speed = new Vector2(-2,0);
+			speed = new Vector2(-2, 0);
 		}
-		offset = new Vector2(0,0);
+		offset = new Vector2(0, 0);
 		texture = new Texture(Gdx.files.internal("zemisileshue.png"));
 		sprite = new Sprite(texture);
 	}
-	
-	public void setOffset(Vector2 v){
+
+	public void setOffset(Vector2 v) {
 		offset = v;
 	}
-	public Vector2 getPosition(){		
+
+	public Vector2 getPosition() {
 		return position;
 	}
-	
-	public void act(){
-		position.add(speed);		
+
+	public void act() {
+		position.add(speed);
+
+		/*
+		 * public void act('target'){
+		 * 
+		 * if (target == inRange()){ 				boolean inRange, area around thrower.
+		 * 
+		 * position.add(projectileArc('target')); 	projectileArc returns Vector2,
+		 * 											calculates the angle to throw projectiles. 
+		 * 											Gravity, volocity, targetposition.
+		 * 
+		 * 
+		 * 
+		 * }
+		 * 
+		 * 
+		 * 
+		 * 
+		 * }
+		 */
+
 	}
-	
-	public Rectangle getRectangle(){
-		return new Rectangle(position.x, position.y, texture.getWidth(), texture.getHeight());
+
+	public Rectangle getRectangle() {
+		return new Rectangle(position.x, position.y, texture.getWidth(),
+				texture.getHeight());
 	}
-	
-	public Texture getTexture(){
+
+	public Texture getTexture() {
 		return texture;
 	}
+
 	public Sprite getSprite() {
 		sprite.setX(position.x + offset.x);
 		sprite.setY(position.y + offset.y);
 		return sprite;
 	}
-	
 
 }
